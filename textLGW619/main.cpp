d0c7893908bdb1c22fb434dc865a7cdbc443efab
20171105136 <1274899067@qq.com>
2019-06-19 20:04:07 +0800

    Initial Commit

---
+++ textLGW619/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  textLGW619
+//
+//  Created by s20171105136 on 2019/6/19.
+//  Copyright © 2019 s20171105136. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
